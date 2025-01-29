363c3d825b035043ad24368e982739d8902e6574
Piotr Oleksiuk <po83516@stud.uws.edu.pl>
2025-01-29 19:03:06 +0100

    Aktualizacja projektu

--- QuizApp/Controllers/QuizController.cs
+++ QuizApp/Controllers/QuizController.cs
@@ -1,189 +1,163 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using QuizApp.Data;
 using QuizApp.Models;
 
-public class QuizController : Controller
+namespace QuizApp.Controllers
 {
-    private readonly QuizAppContext _context;
-
-    public QuizController(QuizAppContext context)
+    public class QuizController : Controller
     {
-        _context = context;
-    }
+        private readonly QuizAppContext _context;
 
-    // GET: Quizzes
-    public async Task<IActionResult> Index()
-    {
-        var quizzes = await _context.Quiz.ToListAsync();
-        return View(quizzes);
-    }
-
-    // GET: Quizzes/Details/5
-    public async Task<IActionResult> Details(int? id)
-    {
-        if (id == null)
+        public QuizController(QuizAppContext context)
         {
-            return NotFound();
+            _context = context;
         }
 
-        var quiz = await _context.Quiz
-            .Include(q => q.Pytania) // Eager loading for questions
-            .FirstOrDefaultAsync(m => m.Id == id);
-
-        if (quiz == null)
+        // GET: Quiz
+        public async Task<IActionResult> Index()
         {
-            return NotFound();
+              return _context.Quiz != null ? 
+                          View(await _context.Quiz.ToListAsync()) :
+                          Problem("Entity set 'QuizAppContext.Quiz'  is null.");
         }
 
-        return View(quiz);
-    }
-
-    // GET: Quizzes/Create
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-    // POST: Quizzes/Create
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("Id,Tytul,CzasTrwania")] Quiz quiz)
-    {
-        if (ModelState.IsValid)
+        // GET: Quiz/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
-            _context.Add(quiz);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-        return View(quiz);
-    }
-
-    // GET: Quizzes/Edit/5
-    public async Task<IActionResult> Edit(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-        var quiz = await _context.Quiz.FindAsync(id);
-        if (quiz == null)
-        {
-            return NotFound();
-        }
-        return View(quiz);
-    }
-
-    // POST: Quizzes/Edit/5
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int id, [Bind("Id,Tytul,CzasTrwania")] Quiz quiz)
-    {
-        if (id != quiz.Id)
-        {
-            return NotFound();
-        }
-
-        if (ModelState.IsValid)
-        {
-            try
+            if (id == null || _context.Quiz == null)
             {
-                _context.Update(quiz);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return NotFound();
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!QuizExists(quiz.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-        }
-        return View(quiz);
-    }
 
-    // GET: Quizzes/Delete/5
-    public async Task<IActionResult> Delete(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-        var quiz = await _context.Quiz
-            .Include(q => q.Pytania) // Eager loading for questions
-            .FirstOrDefaultAsync(m => m.Id == id);
-
-        if (quiz == null)
-        {
-            return NotFound();
-        }
-
-        return View(quiz);
-    }
-
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteConfirmed(int id)
-    {
-        if (_context.Quiz == null)
-        {
-            return Problem("Entity set 'QuizAppContext.Quiz'  is null.");
-        }
-        var quiz = await _context.Quiz.FindAsync(id);
-        if (quiz != null)
-        {
-            _context.Quiz.Remove(quiz);
-            await _context.SaveChangesAsync();
-        }
-
-        return RedirectToAction(nameof(Index));
-    }
-
-    private bool QuizExists(int id) // Metoda zdefiniowana w klasie QuizController
-    {
-        return (_context.Quiz?.Any(e => e.Id == id)).GetValueOrDefault();
-    }
-
-    public async Task<IActionResult> CreateQuestion(int quizId)
-    {
-        var quiz = await _context.Quiz.FindAsync(quizId);
-        if (quiz == null)
-        {
-            return NotFound();
-        }
-        var question = new Pytanie { QuizId = quizId };
-        return View(question);
-    }
-
-   
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> CreateQuestion(int quizId, Pytanie pytanie)
-    {
-        if (ModelState.IsValid)
-        {
-            var quiz = await _context.Quiz.FindAsync(quizId);
+            var quiz = await _context.Quiz
+                .FirstOrDefaultAsync(m => m.Id == id);
             if (quiz == null)
             {
                 return NotFound();
             }
-            pytanie.QuizId = quizId; // Ustawienie QuizId w modelu Pytanie
-            quiz.Pytania.Add(pytanie);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Details), new { id = quizId });
+
+            return View(quiz);
         }
-        return View(pytanie);
+
+        // GET: Quiz/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Quiz/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Tytul,CzasTrwania")] Quiz quiz)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(quiz);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(quiz);
+        }
+
+        // GET: Quiz/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Quiz == null)
+            {
+                return NotFound();
+            }
+
+            var quiz = await _context.Quiz.FindAsync(id);
+            if (quiz == null)
+            {
+                return NotFound();
+            }
+            return View(quiz);
+        }
+
+        // POST: Quiz/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Tytul,CzasTrwania")] Quiz quiz)
+        {
+            if (id != quiz.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(quiz);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!QuizExists(quiz.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(quiz);
+        }
+
+        // GET: Quiz/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Quiz == null)
+            {
+                return NotFound();
+            }
+
+            var quiz = await _context.Quiz
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (quiz == null)
+            {
+                return NotFound();
+            }
+
+            return View(quiz);
+        }
+
+        // POST: Quiz/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Quiz == null)
+            {
+                return Problem("Entity set 'QuizAppContext.Quiz'  is null.");
+            }
+            var quiz = await _context.Quiz.FindAsync(id);
+            if (quiz != null)
+            {
+                _context.Quiz.Remove(quiz);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool QuizExists(int id)
+        {
+          return (_context.Quiz?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
     }
 }
--- QuizApp/Migrations/20250127195751_InitialCreate.Designer.cs
+++ QuizApp/Migrations/20250127195751_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuizApp.Data;
 
@@ -11,9 +12,11 @@ using QuizApp.Data;
 namespace QuizApp.Migrations
 {
     [DbContext(typeof(QuizAppContext))]
-    partial class QuizAppContextModelSnapshot : ModelSnapshot
+    [Migration("20250127195751_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
